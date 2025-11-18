b01ec4395cf2efe58a5898ac6f75bc989b8e1cca
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2025-11-18 15:36:12 +0100

    fix: don't crash when CF is down...

--- src/service-worker/index.ts
+++ src/service-worker/index.ts
@@ -1,7 +1,7 @@
 import { clientsClaim, skipWaiting } from 'workbox-core'
-import { cleanupOutdatedCaches, precacheAndRoute, PrecacheFallbackPlugin } from 'workbox-precaching'
+import { cleanupOutdatedCaches, matchPrecache, precacheAndRoute, PrecacheFallbackPlugin } from 'workbox-precaching'
 import { registerRoute, Route } from 'workbox-routing'
-import { NetworkFirst } from 'workbox-strategies'
+import { NetworkOnly } from 'workbox-strategies'
 
 import { build, files, prerendered, version } from '$service-worker'
 
@@ -13,5 +13,14 @@ clientsClaim()
 skipWaiting()
 
 registerRoute(new Route(({ request }) => request.mode === 'navigate',
-  new NetworkFirst({ plugins: [new PrecacheFallbackPlugin({ fallbackURL })] })
+  new NetworkOnly({
+    plugins: [new PrecacheFallbackPlugin({ fallbackURL }),
+      {
+        async fetchDidSucceed ({ response }) {
+          if (response.ok) return response
+
+          return await matchPrecache(fallbackURL) ?? response
+        }
+      }]
+  })
 ))
