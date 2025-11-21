66beaf8fe0ef0ec332f5c14786d24bdbf6cc577b
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2025-11-21 02:36:44 +0100

    fix: changelog

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,7 @@ import { writable } from 'simple-store-svelte'
 
 import type { Media } from './modules/anilist'
 
-export const COMMITS_URL = 'https://api.github.com/repos/hayase-app/ui/commits'
+export const COMMITS_URL = 'https://api.github.com/repos/hayase-app/interface/commits'
 export const WEB_URL = 'https://hayase.watch'
 export const SETUP_VERSION = 3
 
