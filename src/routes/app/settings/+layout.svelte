eb7fdf0bca82d6d71e681f435eee5dc38fd97737
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2026-01-15 23:26:47 +0100

    fix: make settings UI center

--- src/routes/app/settings/+layout.svelte
+++ src/routes/app/settings/+layout.svelte
@@ -39,15 +39,17 @@
 </script>
 
 <div class='p-3 md:p-10 md:pb-0 pb-0 w-full h-full flex flex-col'>
-  <div class='space-y-0.5'>
-    <h2 class='text-2xl font-bold'>Settings</h2>
-    <p class='text-muted-foreground'>
-      Manage your app settings, preferences and accounts.
-    </p>
+  <div class='flex justify-center'>
+    <div class='space-y-0.5 lg:max-w-[1180px] w-full'>
+      <h2 class='text-2xl font-bold'>Settings</h2>
+      <p class='text-muted-foreground'>
+        Manage your app settings, preferences and accounts.
+      </p>
+    </div>
   </div>
-  <Separator class='my-3 md:my-6' />
-  <div class='flex flex-col lg:flex-row gap-x-12 grow min-h-0'>
-    <aside class='lg:grow lg:max-w-60 flex flex-col'>
+  <Separator class='my-3 md:my-6 max-w-[1180px] mx-auto' />
+  <div class='flex flex-col lg:flex-row gap-x-12 grow min-h-0 overflow-y-scroll justify-center flex-shrink-0'>
+    <aside class='lg:grow lg:max-w-60 flex flex-col sticky top-0'>
       <SettingsNav {items} />
       <div class='mt-auto text-xs text-muted-foreground px-4 sm:px-2 py-3 md:py-5 flex flex-row lg:flex-col font-light gap-0.5 gap-x-4 flex-wrap'>
         <div>Interface v{version}</div>
@@ -63,7 +65,7 @@
         {/if}
       </div>
     </aside>
-    <div class='flex-1 overflow-y-scroll pb-40' use:dragScroll>
+    <div class='pb-40' use:dragScroll>
       <slot />
     </div>
   </div>
