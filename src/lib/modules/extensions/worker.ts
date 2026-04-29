d460429088aea12315d943c9251d058638ccb2da
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2026-04-29 23:59:51 +0200

    feat: CORS support for iOS

--- src/lib/modules/extensions/worker.ts
+++ src/lib/modules/extensions/worker.ts
@@ -1,7 +1,22 @@
 import { finalizer } from 'abslink'
 import { expose } from 'abslink/w3c'
 
+import { SUPPORTS } from '../settings'
+
 import type { NZBorURLSource, SearchFunction, SearchOptions, TorrentQuery, TorrentSource } from './types'
+
+const _fetch = SUPPORTS.isIOS
+  ? (input: RequestInfo | URL, init?: RequestInit) => {
+      if (typeof input === 'string') {
+        input = input.replace(/^https?:\/\//, 'cors://')
+      } else if (input instanceof URL) {
+        input = new URL(input.toString().replace(/^https?:\/\//, 'cors://'))
+      } else if (input instanceof Request) {
+        input = new Request(input.url.replace(/^https?:\/\//, 'cors://'), input)
+      }
+      return fetch(input, init)
+    }
+  : fetch
 
 export default expose({
   mod: null as unknown as Promise<(TorrentSource | NZBorURLSource) & { url: string }>,
@@ -31,22 +46,22 @@ export default expose({
   },
 
   async single (query: TorrentQuery, options?: SearchOptions): ReturnType<SearchFunction> {
-    const queryWithFetch = { ...query, fetch }
+    const queryWithFetch = { ...query, fetch: _fetch }
     return await ((await this.mod) as TorrentSource).single(queryWithFetch, options)
   },
 
   async batch (query: TorrentQuery, options?: SearchOptions): ReturnType<SearchFunction> {
-    const queryWithFetch = { ...query, fetch }
+    const queryWithFetch = { ...query, fetch: _fetch }
     return await ((await this.mod) as TorrentSource).batch(queryWithFetch, options)
   },
 
   async movie (query: TorrentQuery, options?: SearchOptions): ReturnType<SearchFunction> {
-    const queryWithFetch = { ...query, fetch }
+    const queryWithFetch = { ...query, fetch: _fetch }
     return await ((await this.mod) as TorrentSource).movie(queryWithFetch, options)
   },
 
   async query (hash: string, options?: SearchOptions) {
-    return await ((await this.mod) as NZBorURLSource).query(hash, options, fetch)
+    return await ((await this.mod) as NZBorURLSource).query(hash, options, _fetch)
   },
 
   async test () {
