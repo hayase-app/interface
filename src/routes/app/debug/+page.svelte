8fc0aef5a2d73809887c6208080e2e33318c1ef6
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2026-05-18 15:33:38 +0200

    fix: expose av1

--- src/routes/app/debug/+page.svelte
+++ src/routes/app/debug/+page.svelte
@@ -74,6 +74,7 @@
       'video/mp4; codecs="avc1.42E01E"',
       'video/mp4; codecs="avc1.4D401E"',
       'video/mp4; codecs="hev1.1.6.L93.B0"',
+      'video/mp4; codecs="av01.0.05M.08"',
       'audio/mp4; codecs="ac-3"',
       'audio/mp4; codecs="dtsc"',
       'audio/mp4; codecs="truehd"',
