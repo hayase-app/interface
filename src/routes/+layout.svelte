f503858e6aac49aa407528679c73ee8184affa34
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2026-07-12 23:50:21 +0200

    perf: optimise view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,22 +25,22 @@
 
   const displayThresholdMs = 150
 
-  onNavigate(({ to, complete, delta }) => {
-    if (
-      !document.startViewTransition ||
-      SUPPORTS.isUnderPowered ||
-      (SUPPORTS.isIOS && delta != null) || // iOS has their own animations for back/forward navigation that conflict with view transitions, sasuga Apple
-      fullscreenElement || // chrome can hang when trying to do view transitions while in fullscreen
-      (to?.route.id === '/app/player' && SUPPORTS.isMobile) // same as above, but only on mobile since only it forces fullscreen
-    ) return
+  if (document.startViewTransition && !SUPPORTS.isUnderPowered) {
+    onNavigate(({ to, complete, delta }) => {
+      if (
+        (SUPPORTS.isIOS && delta != null) || // iOS has their own animations for back/forward navigation that conflict with view transitions, sasuga Apple
+        fullscreenElement || // chrome can hang when trying to do view transitions while in fullscreen
+        (to?.route.id === '/app/player' && SUPPORTS.isMobile) // same as above, but only on mobile since only it forces fullscreen
+      ) return
 
-    return new Promise((resolve) => {
-      document.startViewTransition(() => {
-        resolve()
-        return complete
+      return new Promise((resolve) => {
+        document.startViewTransition(() => {
+          resolve()
+          return complete
+        })
       })
     })
-  })
+  }
 
   $: scale = SUPPORTS.isAndroidTV ? $settings.uiScale / devicePixelRatio : SUPPORTS.isMobile ? $settings.uiScale : 1
 
